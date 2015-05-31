9c4bc312a5ca6d6cd8d3a8cca86c95542c047c14
zf Gong <i@coxxs.com>
2015-05-31 12:26:36 +0800

    test

--- js/config.js
+++ js/config.js
@@ -7,6 +7,10 @@ var __apiKeys = [
 	'm776858321-486827042ed7a7f7b8952a05', //Auth B
 	'm776858322-0e2c3f842139c06f56f7f56a', //Auth C
 	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
+	'm776309725-9c9cbf53553cdfc7d22f9904',
+	'm776309723-86ee2d0d0992b8d746e58b3f',
+	'm776309726-9d134b679362da8ff24b31ae',
+	'm776625241-f63c5a5e6cd4933791a8869a'
 ];
 
 // refresh interval (in seconds)
