5c9d230412f01557d0445977ba9f263f74aedd49
zf Gong <i@coxxs.com>
2015-05-31 08:48:15 +0800

    更新Key

--- js/config.js
+++ js/config.js
@@ -1,5 +1,8 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
+	'm776858319-7c829d2c3e250f85776f588e', //BBS
+	'm776858325-1ddf7c7e98ce56609325db3b', //Doku
+	'm776858327-19b31e671f6b217b8ded96c6', //Me
 	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
 	'm776858321-486827042ed7a7f7b8952a05', //Auth B
 	'm776858322-0e2c3f842139c06f56f7f56a', //Auth C
@@ -12,4 +15,4 @@ var __refresh = 300;
 // the default language
 // set false to disable language support and show only english,
 // or set null to use visitor's browser language
-var __language = null;
+var __language = 'zh';
