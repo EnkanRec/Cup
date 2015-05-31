0505b16670895bd6c410cdd1dd667885dfca9b93
zf Gong <i@coxxs.com>
2015-05-31 08:44:59 +0800

    更新Key

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ var __apiKeys = [
 	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
 	'm776858321-486827042ed7a7f7b8952a05', //Auth B
 	'm776858322-0e2c3f842139c06f56f7f56a', //Auth C
+	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
 ];
 
 // refresh interval (in seconds)
