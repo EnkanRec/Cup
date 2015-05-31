ec2bdeae8f0344c8e111a03ec5c6f7a48a94ed55
zf Gong <i@coxxs.com>
2015-05-31 08:43:03 +0800

    更新Key

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776858320-7b44f9ac136f69ac3e356e8e',
+	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
+	'm776858321-486827042ed7a7f7b8952a05', //Auth B
+	'm776858322-0e2c3f842139c06f56f7f56a', //Auth C
 ];
 
 // refresh interval (in seconds)
