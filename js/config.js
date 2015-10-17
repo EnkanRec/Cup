842e4e0fe7c448f5fda21858897d1f7ae6763944
zf Gong <i@coxxs.com>
2015-10-18 01:27:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,11 +4,12 @@ var __apiKeys = [
 	'm776858325-1ddf7c7e98ce56609325db3b', //Doku
 	'm776858327-19b31e671f6b217b8ded96c6', //Me
 	'm776858407-a704cd548fb5c15db5b625f9', //Update
+	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
+	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
 	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
 	'm776858321-486827042ed7a7f7b8952a05', //Auth B
 	'm776858322-0e2c3f842139c06f56f7f56a', //Auth C
 	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
-	'm777138853-9ba502b46477b46d6998b115'  //Auth E
 ];
 
 // refresh interval (in seconds)
